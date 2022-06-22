d6c002a15422083d9b29c3db8449c82c7aa069c9
huynhtancuong <huynhtancuong.hcm@gmail.com>
2022-06-22 23:53:26 +0300

    writing node using python OOP

--- src/my_py_pkg/my_py_pkg/my_first_node.py
+++ src/my_py_pkg/my_py_pkg/my_first_node.py
@@ -7,6 +7,11 @@ class MyNode(Node):
     def __init__(self):
         super().__init__("py_test")
         self.get_logger().info("Hello ROS2")
+        self.get_logger().info("Creating timer")
+        self.create_timer(0.5, self.timer_callback)
+    
+    def timer_callback(self):
+        self.get_logger().info("Hello ROS2")
 
 def main(args=None):
     rclpy.init(args=args)
