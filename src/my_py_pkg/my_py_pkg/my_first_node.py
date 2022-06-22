9bb76f1bc896c29a634f7c2d50d960dd103921aa
huynhtancuong <huynhtancuong.hcm@gmail.com>
2022-06-22 23:34:13 +0300

    writing node using python OOP

--- src/my_py_pkg/my_py_pkg/my_first_node.py
+++ src/my_py_pkg/my_py_pkg/my_first_node.py
@@ -2,11 +2,16 @@
 import rclpy
 from rclpy.node import Node
 
+class MyNode(Node):
+
+    def __init__(self):
+        super().__init__("py_test")
+        self.get_logger().info("Hello ROS2")
+
 def main(args=None):
     rclpy.init(args=args)
     # The node is not executable, the node is created inside the file
-    node = Node("py_test") # the name of the node is not the name of the file
-    node.get_logger().info("Hello ROS2")
+    node = MyNode()
     rclpy.spin(node) # wait for callback function
     rclpy.shutdown()
 
